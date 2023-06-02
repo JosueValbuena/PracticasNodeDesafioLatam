01f8e2f3b6b5de231d63b6c9462574bbb1dff0b8
JosueValbuena <joshvlbn@gmail.com>
2023-06-02 15:29:54 -0400

    practica

--- semana1.js
+++ semana1.js
@@ -1,1 +1,65 @@
-console.log("hola");
+// mostrar mensajes en consola desde la terminal---
+
+/* console.log("hola");
+
+const fecha = new Date(); */
+
+// console.log(fecha);
+
+
+// creando archivos desde la terminal---
+
+/* const fs = require("fs");
+
+const tareas = `
+Tarea 1.
+Tarea 2.
+Tarea 3.
+`
+
+fs.writeFileSync("tareas.txt", tareas); */
+
+/* const fs = require("fs");
+
+const tareas = [
+    {text: "tarea 1"},
+    {text: "tarea 2"},
+    {text: "tarea 3"},
+]
+
+fs.writeFileSync("tareas.json", JSON.stringify(tareas)); */
+
+/* const fs = require("fs");
+
+const word = "Creando un archivo Word.doc con Node"
+
+fs.writeFileSync("document.doc", word); */
+
+/* const fs = require("fs");
+
+const excel = "excel1 excel2, excel3";
+
+fs.writeFileSync("excel.xls", excel); */
+
+const fs = require("fs");
+
+const autos = [
+    {
+    marca: 'Susuki',
+    modelo: 'Kicks',
+    },
+    {
+    marca: 'Toyota',
+    modelo: 'Corola',
+    },
+    {
+    marca: 'GAC',
+    modelo: 'GS4',
+    },
+    {
+    marca: 'Changan',
+    modelo: 'CS35 Plus',
+    }
+    ]
+
+fs.writeFileSync("autos.txt", JSON.stringify(autos));
