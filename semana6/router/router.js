1f8e19b33e45e1b830b1b373c7a1227e7fc65e48
JosueValbuena <joshvlbn@gmail.com>
2023-07-12 15:19:24 -0400

    avance en semana 6

--- semana6/router/router.js
+++ semana6/router/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const { getUsuarios, getEventos, verificarCredenciales } = require("../consultas");
 
@@ -18,23 +19,30 @@ router.get("/usuarios", async (req, res) => {
 })
 
 router.get("/eventos", async (req, res) => {
-    const eventos = await getEventos();
-    res.json(eventos);
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.SECRET_KEY);
+        const eventos = await getEventos();
+        res.json(eventos);
+    } catch (error) {
+        res.status(error.code || 500).json({message: "usuario no autorizado"});
+    }
 })
 
 router.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
         await verificarCredenciales(email, password);
-        const token = jwt.sign({ email }, "az_AZ")
-        res.send(token)
+        const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: "1h" });
+        res.send(token);
+        console.log("usuario autenticado correctamente");
     } catch (error) {
         console.log(error);
         res.status(error.code || 500).send(error)
     }
 })
 
-router.delete("/eventos/:id", (req, res) => {
+router.delete("/eventos/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const Authorization = req.header("Authorization");
